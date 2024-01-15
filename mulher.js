bfa9a214662f6f743d37c27641bbb2eab0d4b625
varaujo25 <vanaraujo25@gmail.com>
2024-01-15 14:23:08 -0300

    insere a nova versao do projeoto com o mongodb

--- mulher.js
+++ mulher.js
@@ -8,7 +8,7 @@ function mostraMulher(request, response) {
     response.json({
         nome: 'Vanessa Araujo',
         imagem: 'https://media.licdn.com/dms/image/D4E35AQEo0xfa2EeUCA/profile-framedphoto-shrink_200_200/0/1700600423586?e=1704283200&v=beta&t=dSzne5LEzK8ZColyxo66oc2I-2jXWMcNaKTv7ZuGPWQ',
-        minibio: 'Empreendedora e Desenvolvdora'
+        minibio: 'Empreendedora e Desenvolvedora'
   })
 }
 
--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,108 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const cors = require('cors') //aqui estou trazendo o pacote cor que permite consumir esta api no front-end
 
-const app = express()
-const porta = 3333
+const conectaBancoDeDados = require('./bancoDeDados');
+conectaBancoDeDados();
 
-const mulheres = [
-    {
-        nome: 'Vanessa Araujo',
-        imagem: 'https://media.licdn.com/dms/image/D4E35AQEo0xfa2EeUCA/profile-framedphoto-shrink_200_200/0/1700600423586?e=1704283200&v=beta&t=dSzne5LEzK8ZColyxo66oc2I-2jXWMcNaKTv7ZuGPWQ',
-        minibio:  'Empreendedora e Desenvolvedora'
-    },
-    {
-        nome: 'Ines Araujo',
-        imagem: 'https://media.licdn.com/dms/image/D4E35AQEo0xfa2EeUCA/profile-framedphoto-shrink_200_200/0/1700600423586?e=1704283200&v=beta&t=dSzne5LEzK8ZColyxo66oc2I-2jXWMcNaKTv7ZuGPWQ',
-        minibio: 'Criação e Produção'
-    }, 
-    {
-        nome: 'Mariana Araujo',
-        imagem: 'https://media.licdn.com/dms/image/D4E35AQEo0xfa2EeUCA/profile-framedphoto-shrink_200_200/0/1700600423586?e=1704283200&v=beta&t=dSzne5LEzK8ZColyxo66oc2I-2jXWMcNaKTv7ZuGPWQ',
-        minibio: 'Vendedora'
+const Mulher = require('./mulherModel');
+const app = express();
+const porta = 3333;
+
+app.use(express.json());
+app.use(cors())
+
+// Rota GET para mostrar todas as mulheres
+app.get('/mulheres', mostraMulheres);
+
+// Rota POST para criar uma nova mulher
+app.post('/mulheres', criaMulher);
+
+// Rota PATCH para corrigir informações de uma mulher específica
+app.patch('/mulheres/:id', corrigeMulher);
+
+// Rota DELETE para deletar uma mulher específica
+app.delete('/mulheres/:id', deletaMulher);
+
+// Função para corrigir mulher
+async function corrigeMulher(request, response) {
+  try {
+    const mulherEncontrada = await Mulher.findById(request.params.id);
+
+    if (!mulherEncontrada) {
+      return response.status(404).json({ erro: 'Mulher não encontrada' });
     }
-]
 
-function mostraMulheres(requeste, response) {
-    response.json(mulheres)
+    // Atualizar propriedades conforme necessário
+    if (request.body.nome) {
+      mulherEncontrada.nome = request.body.nome;
+    }
+
+    if (request.body.minibio) {
+      mulherEncontrada.minibio = request.body.minibio;
+    }
+
+    if (request.body.imagem) {
+      mulherEncontrada.imagem = request.body.imagem;
+    }
+
+    if (request.body.citacao) {
+      mulherEncontrada.citacao = request.body.citacao;
+    }
+
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save();
+    response.json(mulherAtualizadaNoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+    response.status(500).json({ erro: 'Erro interno do servidor' });
+  }
 }
 
+// Função para deletar mulher
+async function deletaMulher(request, response) {
+  try {
+    await Mulher.findByIdAndDelete(request.params.id);
+    response.json({ mensagem: 'Mulher deletada com sucesso!' });
+  } catch (erro) {
+    console.log(erro);
+    response.status(500).json({ erro: 'Erro interno do servidor' });
+  }
+}
+
+// Função para mostrar todas as mulheres
+async function mostraMulheres(request, response) {
+  try {
+    const mulheresVindasDoBancoDeDados = await Mulher.find();
+    response.json(mulheresVindasDoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+    response.status(500).json({ erro: 'Erro interno do servidor' });
+  }
+}
+
+// Função para criar uma nova mulher
+async function criaMulher(request, response) {
+  const novaMulher = new Mulher({
+    nome: request.body.nome,
+    imagem: request.body.imagem,
+    minibio: request.body.minibio,
+    citacao: request.body.citacao
+  });
+  try {
+    const mulherCriada = await novaMulher.save();
+    response.status(201).json(mulherCriada);
+  } catch (erro) {
+    console.log(erro);
+    response.status(500).json({ erro: 'Erro interno do servidor' });
+  }
+}
+
+// Função para mostrar a porta
 function mostraPorta() {
-    console.log("Servidor criado e rodando na porta ", porta)
+  console.log("Servidor criado e rodando na porta", porta);
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+// Use diretamente a função app.post para configurar a rota POST /mulheres
+app.post('/mulheres', criaMulher);
+
+app.listen(porta, mostraPorta);
